c1479d76997127a2dc55573d2f4c5fc597f045d0
Adam Wieczorek <adamwieczorek034@gmail.com>
2023-02-13 14:26:30 +0100

    adding and removing functions added

--- Lista zadań/js/script.js
+++ Lista zadań/js/script.js
@@ -10,20 +10,6 @@
         },
     ]
 
-    const render = () => {
-        let htmlString = "";
-        
-        for (const task of tasks) {
-            htmlString += `
-            <li${task.done ? " class=\"section__listItem\"" : ""}>
-                ${task.content}
-            </li>
-            `;
-        }
-
-        document.querySelector(".js-list").innerHTML = htmlString
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
@@ -31,6 +17,35 @@
 
         render();
     }
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+            render()
+    }
+
+    const render = () => {
+        let htmlString = "";
+        
+        for (const task of tasks) {
+            htmlString += `
+            <li${task.done ? " class=\"section__listItem\"" : ""}>
+                ${task.content} <button class="js-remove">...</button>
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-list").innerHTML = htmlString
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index)
+           });
+        });
+    };
+
+    
 
     const onFormSubmit = (event) => {
         event.preventDefault();
